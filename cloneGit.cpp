d47593de4b5939d214f08976ec236ffe823675de
giovanaas <azevedogiovana309@gmail.com>
2025-03-17 10:43:57 -0300

    AULA3

--- cloneGit.cpp
+++ cloneGit.cpp
@@ -10,7 +10,7 @@ int main() {
     cout << "\033[1;32m*   \033[1;33m PARABÉNS 34DS!                     \033[1;32m          *\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;32m*   \033[1;36mVocê clonou um repositório público do Git     \033[1;32m*\033[0m\n";
-    cout << "\033[1;32m*   \033[1;36mpara o seu repositório local!                 \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mpara o seu repositório local! VAI RAPOSA      \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;34m***************************************************\033[0m\n";
 
